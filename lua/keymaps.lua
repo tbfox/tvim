f4934b2d96e2f7f36eb7b8d0c0e419d8e59f36fd
tristanbarrow <tristanmarkbarrow@gmail.com>
2025-06-27 22:02:59 -0600

    fixed windown creation, added window navigation

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -9,18 +9,36 @@ vim.keymap.set("n", "-", "<CMD>Oil<CR>", {
     desc = "Explore w/ Minus"
 })
 
-vim.keymap.set('n', "<Leader>wnl", "<CMD>vsplit<CR>", {
-    desc = "[W]indow - [N]ew - [L]eft"
+-- Window Creation
+vim.keymap.set('n', "<Leader>wnh", "<CMD>vsplit<CR>", {
+    desc = "[W]indow - [N]ew - (vim direction left)"
 })
 
-vim.keymap.set('n', "<Leader>wnr", "<CMD>rightbelow vsplit<CR>", {
-    desc = "[W]indow - [N]ew - [R]ight"
+vim.keymap.set('n', "<Leader>wnj", "<CMD>rightbelow split<CR>", {
+    desc = "[W]indow - [N]ew - (vim direction down)"
 })
 
-vim.keymap.set('n', "<Leader>wnu", "<CMD>split<CR>", {
-    desc = "[W]indow - [N]ew - [U]p"
+vim.keymap.set('n', "<Leader>wnk", "<CMD>split<CR>", {
+    desc = "[W]indow - [N]ew - (vim direction up)"
 })
 
-vim.keymap.set('n', "<Leader>wnd", "<CMD>rightbelow split<CR>", {
-    desc = "[W]indow - [N]ew - [D]own"
+vim.keymap.set('n', "<Leader>wnl", "<CMD>rightbelow vsplit<CR>", {
+    desc = "[W]indow - [N]ew - (vim direction right)"
+})
+
+-- Window Navigation
+vim.keymap.set('n', "<Leader>wh", "<C-w>h", {
+    desc = "Navigage [W]indow - (vim direction left)"
+})
+
+vim.keymap.set('n', "<Leader>wj", "<C-w>j", {
+    desc = "Navigage [W]indow - (vim direction down)"
+})
+
+vim.keymap.set('n', "<Leader>wk", "<C-w>k", {
+    desc = "Navigage [W]indow - (vim direction up)"
+})
+
+vim.keymap.set('n', "<Leader>wl", "<C-w>l", {
+    desc = "Navigage [W]indow - (vim direction right)"
 })
