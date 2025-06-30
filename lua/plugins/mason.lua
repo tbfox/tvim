fa0ebc672bf4c4ec53f96b76aae5f566da89ff78
tristanbarrow <tristanmarkbarrow@gmail.com>
2025-06-30 07:33:59 -0600

    remove incorrect mason plugin

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -12,7 +12,6 @@ local function config()
     require("mason-lspconfig").setup({
         ensure_installed = {
             "lua_ls",
-            "typescript_language_server",
         },
         on_attach = on_attach,
         handlers = {
