c2d9fa5e37df753cc9f723d4efa41fecec4af05f
tristanbarrow <tristanmarkbarrow@gmail.com>
2025-08-10 21:41:12 -0600

    add lsp keymaps

--- lua/lib/common-lsp-keymaps.lua
+++ lua/lib/common-lsp-keymaps.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-function wrapper()
+local function wrapper()
     vim.api.nvim_set_hl(0, "@markup.raw.block.markdown", { bg = "#000000", })
     vim.lsp.buf.hover()
     -- vim.api.nvim_set_hl(0, "@markup.raw.block.markdown", { bg = "NONE", })
@@ -8,14 +8,15 @@ end
 
 function M.set_keymaps(opts)
     vim.keymap.set('n', '<leader>i', wrapper, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<leader>ci', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<leader>cr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>ct', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>fm', vim.lsp.buf.format, opts)
+    vim.keymap.set('n', '<leader>h', vim.lsp.buf.signature_help, opts)
 end
 
--- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
--- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
--- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
--- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
--- vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
--- vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
--- vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
--- vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help, opts)
 return M
