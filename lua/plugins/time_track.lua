a8b9e0791db4c4db97d0857f5f208c57d9288f59
Tristan Barrow <tristanmarkbarrow@gmail.com>
2025-11-03 13:49:38 -0700

    disable time track by default

--- lua/plugins/time_track.lua
+++ lua/plugins/time_track.lua
@@ -1,7 +1,7 @@
 return {
     {
         dir = require("lib.local-plugin")("time_track.nvim"),
-        enabled = true,
+        enabled = false,
         dependencies = { { "nvim-tree/nvim-web-devicons" } },
         config = function()
             require("time_track").setup()
